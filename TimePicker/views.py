f6f5b8b705cfdf34ac546fccd79a1b48fe671c59
Taranemp <mohammadpoor.t2002@gmail.com>
2025-11-24 06:59:02 -0800

    Implement authentication & permission updates:

--- TimePicker/views.py
+++ TimePicker/views.py
@@ -2,9 +2,17 @@ from rest_framework import viewsets
 from .models import Student,Course,CalendarSlot
 from .serializers import StudentSerializer,CourseSerializer,CalendarSlotSerializer
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import status, views
+from rest_framework.permissions import AllowAny, IsAdminUser
+
 
 
 class StudentViewSet(viewsets.ModelViewSet):
+    def get_permissions(self):
+        if self.action == 'destroy':
+            return [IsAdminUser()]   
+        return [AllowAny()]
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
     
@@ -13,18 +21,38 @@ class StudentViewSet(viewsets.ModelViewSet):
 
     
 class CourseListCreateView(generics.ListCreateAPIView):
+    def get_permissions(self):
+        if self.request.method == 'create':
+            return [IsAdminUser()]
+        return [AllowAny()] 
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
     
 class CourseDetailView(generics.RetrieveDestroyAPIView):
+    def get_permissions(self):
+        if self.request.method == 'DELETE':
+            return [IsAdminUser()]
+        return [AllowAny()]
+
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
     
 class CalendarSlotListCreateView(generics.ListCreateAPIView):
+    def get_permissions(self):
+        if self.request.method == 'create':
+            return [IsAdminUser()]
+        return [AllowAny()] 
     queryset = CalendarSlot.objects.all()
     serializer_class = CalendarSlotSerializer
     
 class CalendarSlotDetailView(generics.RetrieveDestroyAPIView):
+    def get_permissions(self):
+        if self.request.method == 'DELETE':
+            return [IsAdminUser()]
+        return [AllowAny()]
+
     queryset = CalendarSlot.objects.all()
     serializer_class = CalendarSlotSerializer
     
+
+
