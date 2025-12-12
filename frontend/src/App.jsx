2ed99b11b83491e00dd06db04bba3549312cdbd1
Taranemp <mohammadpoor.t2002@gmail.com>
2025-12-12 06:53:11 -0800

    React JS setup — starting point after Bootstrap installation

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import {Button, Stack} from 'react-bootstrap';
+
+function App() {
+
+
+  return (
+    <>
+        <Button variant="success">
+            test
+        </Button>
+    </>
+  )
+}
+
+export default App
